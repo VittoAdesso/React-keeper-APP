5144449f9be4f00eabb6afbe21355440838c86ce
Vittorio Adesso <adessovittorio8@gmail.com>
2022-01-29 23:50:26 +0100

    M

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,5 +1,12 @@
 import NavBar from './components/NavBar/NavBar'; 
 
+// para usar thme importo varios cosas
+
+// import { themeProvider } from 'styled-components
+// import { createTheme, themeProvider, styled } from '@mui/material/styles'
+// import { createTheme } from '@mui/material';
+
+
 function App() {
   return (
     <div >
